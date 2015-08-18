8be6ba8ac2da8026da787e7001365f1c40e390d1
Mark Cameron <mark.cameron@devfactory.ch>
2015-08-18 12:14:50 +0200

    Added show() function to display generated menu on the front

--- src/Menu.php
+++ src/Menu.php
@@ -1,13 +1,29 @@
 <?php namespace Devfactory\Menu;
 
 use Devfactory\Menu\Controllers\MenuController;
+use Devfactory\Menu\Models\Menu as MenuModel;
+use Menu as VespaokenMenu;
 
-class Menu  {
+class Menu {
 
-  public function test() {
-    $c = new MenuController;
-    echo "<pre>";
-    print_r($c);
-    echo "</pre>";
+  public function show($classes = '') {
+    VespaokenMenu::handler('main')->hydrate(function() {
+      return MenuModel::all();
+    }, function($children, $item) {
+      $attributes = array();
+      if($item->external) {
+        $attributes['target'] = '_blank';
+        $parse_url = parse_url($item->url);
+
+        if(!isset($parse_url['scheme'])){
+          $item->url = 'http://' . $item->url;
+        }
+      }
+
+      $children->add($item->url, $item->title, VespaokenMenu::items($item->url), $attributes);
+    });
+
+    return VespaokenMenu::handler('main')->addClass($classes)->render();
   }
+
 }
